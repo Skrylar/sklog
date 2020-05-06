f292619c78bf3296948f1d95e90d9860034d3921
Joshua Cearley <joshua.cearley@gmail.com>
2020-05-05 21:31:50 -0500

    escape keys and values when we need to

--- sklog.nim
+++ sklog.nim
@@ -2,7 +2,13 @@
 proc must_quote_for_logfmt*(input: string): bool =
   ## Checks whether the given string needs to be escaped to print on a logfmt
   ## line.
-  discard # TODO
+  for ch in input:
+    case ch:
+    of 'a'..'z', 'A'..'Z', '0'..'9', '-', '_':
+      continue
+    else:
+      return true
+  return false
 
 proc must_quote_for_json*(input: string): bool =
   ## Returns if a string needs any more work than just surrounding it in
@@ -11,7 +17,17 @@ proc must_quote_for_json*(input: string): bool =
 
 proc quote_for_logfmt*(input: string): string =
   ## Returns a new string which has been safely quoted to echo via logfmt.
-  discard # TODO
+  result = newStringOfCap(input.len)
+
+  result &= "\""
+  for ch in input:
+    case ch:
+    of '"', '\\':
+      result &= '\\'
+      result &= ch
+    else:
+      result &= ch
+  result &= "\""
 
 proc quote_for_json*(input: string): string =
   ## Returns a new string which has been safely quoted to echo via JSON.
@@ -22,14 +38,14 @@ proc log*(values: openarray[(string, string)]) =
   var c = values.high
   for v in values:
     if must_quote_for_logfmt(v[0]):
-      discard # TODO
+      write stdout, quote_for_logfmt(v[0])
     else:
       write stdout, v[0]
 
     write stdout, "="
 
     if must_quote_for_logfmt(v[1]):
-      discard # TODO
+      write stdout, quote_for_logfmt(v[1])
     else:
       write stdout, v[1]
 
@@ -39,4 +55,4 @@ proc log*(values: openarray[(string, string)]) =
 
     flushFile stdout
 
-log({"milk": "toast", "level": "info", "status": "ok"})
+log({"milk": "toast is good", "level": "info", "status": "ok"})
